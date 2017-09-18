d803e9218db9164d1db1491500f1616c0747bdb1
Jack Bevis <jack.bevis@gmail.com>
2017-09-18 15:53:21 -0600

    refactor, file structure

---
+++ src/components/Input.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    Title:
+    <input v-model='title'>
+    <br><br>
+    Body:
+    <input v-model='body'>
+    <br><br>
+    <button @click='submit'>Submit</button>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['add-idea'],
+  data() {
+    return {
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+    generateIdea() {
+      return { title: this.title, body: this.body, quality: 'swill' }
+    },
+    submit() {
+      this.addIdea(this.generateIdea())
+      this.title = ''
+      this.body = ''
+    }
+  }
+}
+</script>
